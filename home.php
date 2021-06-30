d2c69e3647b130ae133524aa5c037d5e90a66a59
akli0027 <73159374+akli0027@users.noreply.github.com>
2021-06-30 23:20:10 +0700

    Update home.php

--- home.php
+++ home.php
@@ -6,23 +6,24 @@ if (!isset($_SESSION['id_pembeli'])){
 }
 
 include 'koneksi.php';
+// Mendapatkan IP pengunjung menggunakan getenv()
 function get_client_ip() {
-  $ipaddress = '';
-  if (isset($_SERVER['HTTP_CLIENT_IP']))
-      $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-  else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-  else if(isset($_SERVER['HTTP_X_FORWARDED']))
-      $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-  else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-      $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-  else if(isset($_SERVER['HTTP_FORWARDED']))
-      $ipaddress = $_SERVER['HTTP_FORWARDED'];
-  else if(isset($_SERVER['REMOTE_ADDR']))
-      $ipaddress = $_SERVER['REMOTE_ADDR'];
-  else
-      $ipaddress = 'UNKNOWN';
-  return $ipaddress;
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'IP tidak dikenali';
+    return $ipaddress;
 }
 $ip_user = get_client_ip();
 $activity="Akses Menu Home";
